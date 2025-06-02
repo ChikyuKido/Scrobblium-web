ae511886f5d421782ce6fd5bfb249424f8e538a0
Kidō <chikyu.kido@posteo.de>
2025-06-02 11:03:11 +0200

    forgot frontend

--- web/src/views/TrackList.vue
+++ web/src/views/TrackList.vue
@@ -1,46 +1,48 @@
 <script setup lang="ts">
+import { shallowRef,ref, onMounted, watch } from "vue"
+import TrackDataTable from "@/components/own/trackTable/TrackDataTable.vue"
+import { trackColumns } from "@/components/own/trackTable/TrackColumns.ts"
+import { checkAuthorization } from "@/lib/utils.js"
+import axiosInstance from "@/axiosInstance.ts"
+import type { TrackData } from "@/dto/TrackData.ts"
 
-import TrackDataTable from "@/components/own/trackTable/TrackDataTable.vue";
-import {trackColumns} from "@/components/own/trackTable/TrackColumns.ts";
-
-const trackData = ref({})
-
-
-
-import {checkAuthorization} from "@/lib/utils.js";
-import {onMounted, ref, watch} from "vue";
-import type {TrackData} from "@/dto/TrackData.ts";
-import axiosInstance from "@/axiosInstance.ts";
+const selectedCombine = ref("track")
+const trackData = shallowRef<TrackData[]>([])
 
 async function fetchData() {
+  const combine = selectedCombine.value
+  if (!combine) return
   try {
-    const res = await axiosInstance.get('/track/list?combine='+selectedCombine.value)
-    const data = res.data
-    trackData.value = data as TrackData[]
-    console.log(trackData.value)
+    const res = await axiosInstance.get(`/track/list?combine=${combine}`)
+    trackData.value = res.data as TrackData[]
   } catch (e) {
-    console.error('Failed to fetch settings', e)
+    console.error("Failed to fetch settings", e)
   }
 }
+
 onMounted(async () => {
-  await checkAuthorization('list')
+  console.time("auth")
+  await checkAuthorization("list")
+  console.timeEnd("auth")
+  console.time("fetch")
   await fetchData()
+  console.timeEnd("fetch")
 })
 
-const selectedCombine = ref('track')
-
-watch(selectedCombine, (val) => {
+watch(selectedCombine, () => {
   fetchData()
 })
-
 </script>
 
 <template>
   <div class="container py-10 mx-auto">
-    <TrackDataTable :columns="trackColumns" :data="trackData" v-model="selectedCombine" />
+    <TrackDataTable
+        :columns="trackColumns"
+        :data="trackData"
+        v-model="selectedCombine"
+    />
   </div>
 </template>
 
 <style scoped>
-
 </style>
